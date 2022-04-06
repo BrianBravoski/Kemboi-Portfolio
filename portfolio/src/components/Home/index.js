b9d0fa672fe085d9605552428a1b06720f707a89
BrianBravoski <brianbravoski28@gmail.com>
2022-04-06 23:50:42 +0300

    changing the style of home and layout

--- portfolio/src/components/Home/index.js
+++ portfolio/src/components/Home/index.js
@@ -1,17 +1,19 @@
 import LogoTitle from '../../assets/images/logo-s.png'
 import {Link} from 'react-router-dom'
+import './index.scss';
+
 const Home =() => {
     return(
         <div className="container home-page">
             <div className="text-zone">
-                <h1> Hi, <br/> I'm
-                <img src={LogoTitle}  alt="developer"/>
-                kemboi
+                <h1> Hi, <br/> I'm<br />
+                {/* <img src={LogoTitle}  alt="developer"/> */}
+                Kemboi
                 <br/>
                 web developer
                 </h1>
-                <h2> Frontend Developer / JavaScript </h2>
-                <Link to="/contact" className='flat-button'></Link>
+                <h2> Frontend Developer | JavaScript </h2>
+                <Link to="/contact" className='flat-button'>CONTACT</Link>
             </div>
 
         </div>
--- portfolio/src/components/Layout/index.js
+++ portfolio/src/components/Layout/index.js
@@ -1,25 +1,25 @@
 import './index.scss';
-import Sidebar from '../Sidebar'
+import Sidebar from '../Sidebar/'
 import { Outlet } from 'react-router-dom';
 import './index.scss'
 
-const Layout = () =>{
-    return(
+const Layout = () => {
+    return (
         <div className='App'>
 
-       <Sidebar/> 
-       <div className='page'>
-           <span className='tags top-tags'>&lt;body&gt;</span>
+            <Sidebar />
+            <div className='page'>
+                <span className='tags top-tags'>&lt;body&gt;</span>
 
-           <Outlet/>
-           <span className='tags bottom-tags'>
-           &lt;/body&gt;
-           <br/>
-           <span className='bottom-tag-html'>&lt;/html&gt;</span> 
-           </span>
-       </div>
-       </div>
+                <Outlet />
+                <span className='tags bottom-tags'>
+                    &lt;/body&gt;
+                    <br />
+                    <span className='bottom-tag-html'>&lt;/html&gt;</span>
+                </span>
+            </div>
+        </div>
     )
 
 }
- export default Layout
+export default Layout
