eb1cb0d7714598fbba309098f9432d8a6730da70
BrianBravoski <brianbravoski28@gmail.com>
2022-04-21 20:54:57 +0300

    minor adjustments

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,18 +4,45 @@ import { Routes, Route , BrowserRouter as Router, useRoutes } from 'react-router
 import Layout from './components/Layout';
 import Home from './components/Home';
 import Contact from './components/contact'
+import About from './components/about'
 
-function App() {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Layout />} />
-        <Route index element={<Home />} />
-        <Route path="/contact" element={<Contact/>}  />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </>
+const App=()=>
+{
+  let element= useRoutes([
+    {
+      path:"/",
+      element:<Layout />
+    },
+    {
+      path:"/contact",
+      element: <Contact/>
+    },
+    {
+      path:"/about",
+      element:<About/>
+    }
+  ])
+
+  return element;
+
+  // return (
+  //   <>
+  //     <Routes>
+  //       <Route path="/" element={<Layout />} />
+  //       <Route index element={<Home />} />
+  //       <Route path="/contact" element={<Contact/>}  />
+  //       <Route path="/contact" element={<Contact />} />
+  //     </Routes>
+  //   </>
+  // )
+}
+const AppWrapper=()=>{
+  return(
+    <Router>
+      <App/>
+    </Router>
   )
 }
 
-export default App;
+
+export default AppWrapper;
--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AppWrapper from './App';
 
 // ReactDOM.render(
 //   <React.StrictMode>
@@ -14,7 +15,7 @@ import reportWebVitals from './reportWebVitals';
 // );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App/>)
+root.render(<AppWrapper/>)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
